14de6a22a54c5c837bf984f7ddcd090876b5d422
Bett <bettpatrick080@gmail.com>
2024-03-25 16:06:04 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1,"Mr. Chocolate",25,"M","white","high tempered",1);
+INSERT INTO bears VALUES(2,"Rowdy",28,"M","dark brown","cool",0);
+INSERT INTO bears VALUES(3,"Tabitha",36,"F","white","calm",1);
+INSERT INTO bears VALUES(4,"Sergeant Brown",50,"M","dark brown","high tempered",0);
+INSERT INTO bears VALUES(5,"Melisa",20,"F","black","naive",1);
+INSERT INTO bears VALUES(6,"Grinch",40,"M","dark brown","cool",1);
+INSERT INTO bears VALUES(7,"Wendy",17,"F","white","calm",1);
+INSERT INTO bears VALUES(8,null,65,"M","black","cool",0);
